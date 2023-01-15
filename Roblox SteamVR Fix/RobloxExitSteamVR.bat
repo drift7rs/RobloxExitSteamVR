bc7edf623187d3c60393c4bbb76fb0f5cc3922f5
drift7rs <66232936+drift7rs@users.noreply.github.com>
2023-01-16 03:54:03 +1100

    Delete Roblox SteamVR Fix directory

--- Roblox SteamVR Fix/RobloxExitSteamVR.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:start
-tasklist /fi "imagename eq RobloxPlayerBeta.exe" | find "RobloxPlayerBeta.exe" >nul
-if %errorlevel%==0 (
-    taskkill /f /im vrmonitor.exe
-)
-timeout /t 1
-goto start
